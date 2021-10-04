03bf63c0e95e62eabc0efb6a350d8df5868f2c61
Daylam Tayari <git@tayari.gg>
2021-10-04 08:10:15 -0700

    Created and completed initialisation script
    Created and completed the initialisation script which performs basic dependency checks and initialises the system for the installation of the dotfiles. Also fixed the execution of the setup.sh script in the dotbot install config.

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is a script which checks the system for specific dependencies and initializes the system for the setup of the dotfiles.
+
+check_git(){
+	if [[ "`git`" != *"These are common Git commands"* ]]; then
+		echo 'Error: Git is NOT installed.'
+		exit 1
+	fi
+}
+
+check_gpg(){
+	if [[ "`gpg`" != *"gpg: WARNING: no command supplied."* ]]; then
+		echo 'Error: GPG is NOT installed.'
+		exit 1
+	fi
+}
+
+
+check_git
+check_gpg
+
+# Execute the dotbot installation script:
+chmod +x install
+./install
