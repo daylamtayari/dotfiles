8010102d794321f17eda76d7639f23de615ba416
Daylam Tayari <git@tayari.gg>
2021-10-03 23:01:39 -0700

    Completely redesigned setup script excluding dependency install
    Completely redesigned the setup script from the ground up and completed all of it excluding the install dependency function which installs a given set of dependency depending on the platform and thus package manager.

--- setup.sh
+++ setup.sh
@@ -4,77 +4,136 @@
 # This scripts needs to be ran by root to fully work otherwise it will only setup the Qubes specific elements.
 
 echo Welcome to tayari\'s dotfiles system setup!
+echo Notice: This tool does not enforce valid inputs. If an input provided is invalid, it will just skip that step.
 
-# Check if this system is a Qubes VM and if so apply the necessary changes:
-read -e -p 'Please enter `y` if this is running on a Qubes VM and `n` otherwise: ' QUBES
-case $QUBES in
-	"y")
-		export QUBES_GPG_DOMAIN=gpg
-		gpg -K
-		qubes-gpg-client -K
-		printf "\n[gpg]\n\tprogram=qubes-gpg-client-wrapper" >> gitconfig
-		printf "\nalias -g gpg=\"qubes-gpg-client-wrapper\"" >> zsh/zshrc
-		;;
-	"n")
-		echo "no for qubes"
-		gpg -K
-		printf "\n[gpg]\n\tprogram=gpg" >> gitconfig
+# Check if the system is a Qubes VM:
+qubes='null'
+if [[ "`uname -a`" == *"qubes"* ]]; then
+	qubes='y'
+else
+	qubes='n'
+fi
+
+# Setup GPG:
+read -e -p 'Please enter `y` if GPG has not been configured and `n` otherwise: ' gpg_configured
+case $gpg_configured in
+	"y"|"Y")
+		case $qubes in
+			'y')
+				export QUBES_GPG_DOMAIN=gpg
+				gpg -K
+				qubes-gpg-client -K
+				printf "\n[gpg]\n\tprogram=qubes-gpg-client-wrapper" >> gitconfig
+				printf "\nalias -g gpg=\"qubes-gpg-client-wrapper\"" >> zsh/zshrc
+				;;
+			'n')
+				gpg -K
+				printf "\n[gpg]\n\tprogram=gpg" >> gitconfig
+				;;
+			*)
+				echo 'error detecting qubes'
+				;;
+		esac ;;
+	"n"|"N")
+		echo 'GPG already configured - skipping.'
 		;;
 	*)
-		echo "Invalid response provided - Skipping"
+		echo 'Invalid input provided for GPG configuration value - skipping.'
 		;;
 esac
 
-# Check which distro is running and act accordingly:
-echo "Please enter which distro is the current system based off:"
-echo "Debian-based: 1; Fedora-based: 2; Arch-based: 3; Red Hat-based: 4; Suse-based: 5."
-read -p "Please enter your selection: " DISTRO
-case $DISTRO in
-	1)
-		sudo apt install git
-		sudo apt install zsh
-		sudo apt install kitty
-		sudo apt install firefox
+# Check if the user wants to install dependencies and has root access:
+
+## Check if the script is being ran as sudo 
+read -e -p 'Do you which to install all dependencies of the dotfiles? (y/n): ' depen
+
+## Ask the user which distro they are running:
+distro=0
+check_distro(){
+	echo "Please enter which distro is the current system based off:"
+	echo "1: Deian-based --- 2: Fedora-based --- 3: Arch-based --- 4: Red Hat-based --- 5: Suse-based."
+	read -p "Please enter your selection: " DISTRO
+	if [[ $DISTRO<1 || $DISTRO>5 ]]; then
+		echo 'Invalid input provided for distro version - skipping dependency installation.'
+	else
+		distro=$DISTRO
+	fi
+}
+
+## Check if the user is able to execute root commands:
+is_root='null'
+check_root(){
+	echo 'To install dependencies, you need to be able to authenticate as root - please authenticate as root:'
+	if [[ "`sudo -l`" == *"(root)"* ]]; then
+		is_root='y'
+		echo 'Authentication as sudo successful - installing dependencies...'
+	else
+		is_root='n'
+		echo 'Authentication as sudo unsuccessful - skipping dependency install.'
+	fi
+}
+
+## Function to install dependencies:
+install_dependencies(){
+	echo ''
+}
+
+## Check if user wants to install dependencies and execute the corresponding functions:
+case $depen in
+	"y"|"Y")
+		check_distro
+		check_root
+		if [ "$is_root" == 'y' ]; then
+			install_dependencies
+		fi
 		;;
-	2)
-		sudo dnf install git
-		sudo dnf install zsh
-		sudo dnf install kitty
-		sudo dnf install firefox
-		;;
-	3)
-		sudo pacman -S git
-		sudo pacman -S zsh
-		sudo pacman -S kitty
-		sudo pacman -S firefox
-		;;
-	4)
-		sudo yum install git
-		sudo yum install zsh
-		sudo yum install kitty
-		sudo yum install firefox
-		;;
-	5)
-		sudo zypper install git
-		sudo zypper install zsh
-		sudo zypper install kitty
-		sudo zypper install firefox
+	"n"|"N")
+		echo 'Dependencies will not be installed.'
 		;;
 	*)
-		echo "Invalid response provided - Skipping"
+		echo 'Invalid input provided for dependency install value - skipping.'
 		;;
 esac
 
-# Set ZSH as the default shell for the system:
-case $QUBES in
-	"y")
-		sudo chsh -s $(which zsh) user
-		sudo chsh -s $(which zsh) # Also setting ZSH as the default shell for root.
-		;;
-	*)
-		chsh -s $(which zsh)
-		;;
-esac
-echo ZSH is now the default shell for the system - Please reboot to apply this change.
+
+# Set ZSH as the default shell of the system:
+
+## Check if ZSH is the default shell:
+zsh_def='null'
+check_shell(){
+	if [[ "${SHELL}" != *"zsh" ]]; then
+		zsh_def='n'
+	fi
+}
+
+## Function to set the ZSH shell depending on whether the system is a Qubes VM:
+shell_qubes(){
+	case $qubes in
+		'y')
+			sudo chsh -s $(which zsh) user
+			sudo chsh -s $(which zsh) # Also setting ZSH as the default shell for root.
+			;;
+		'n')
+			chsh -s $(which zsh)
+			if [[ "$is_root" == "y" ]]; then # Also setting ZSH as the default shell for root.
+				sudo chsh -s $(which zsh) 
+			fi
+			;;
+	esac
+}
+
+## Check if ZSH is even installed and then execute the appropriate functions:
+if ! command -v zsh > /dev/null; then
+	echo 'ZSH is not installed - skipping setting ZSH as default shell.'
+else
+	check_shell
+	if [[ "$zsh_def" == 'n' ]]; then 
+		shell_qubes
+		echo 'ZSH is now the default shell - Please reboot to apply this change.'
+	else
+		echo 'ZSH is already the default shell.'
+	fi
+fi
+
 
 echo Setup script has finished!
