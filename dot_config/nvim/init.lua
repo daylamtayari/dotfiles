0aa81a6669569bd17f20ec51f1c28f6ff72464ba
Daylam Tayari <git@tayari.gg>
2024-04-01 01:10:48 -0500

    Added global vim options to nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -37,3 +37,41 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- Global Options
+
+-- Encoding Options:
+vim.o.encoding = "UTF-8"
+vim.o.fileencoding = "UTF-8"
+
+-- Show relative line numbers and position:
+vim.o.relativenumber = false
+vim.o.number = true
+vim.o.ruler = true
+
+-- Tab settings (4 spaces, tabs are expressed as spaces and auto-indent):
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+
+-- Color and Syntax Highlighting:
+vim.o.termguicolors = true
+vim.o.syntax = "on"
+--vim.o.cursorline=true
+vim.o.showmatch = true
+
+-- Disable line wrapping:
+vim.o.wrap = false
+
+-- Do not hide characters:
+vim.o.conceallevel = 0
+
+-- Search settings:
+vim.o.incsearch = true
+vim.o.ignorecase = true
+
+--Enable tab completion in command mode:
+vim.o.wildmenu = true
+
