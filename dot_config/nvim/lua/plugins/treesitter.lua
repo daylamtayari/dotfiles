6cf22d57a93b7a0553bb652f5e8995ef80724f8a
Daylam Tayari <git@tayari.gg>
2024-06-02 15:01:44 -0500

    Added languages to treesitter and formatting

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,17 +3,33 @@ return {
   opts = function(_, opts)
     vim.treesitter.language.register("markdown", "mdx")
     -- vim.list_extend(opts.highlight.disable, { "tsx" })
+    opts.highlight = { enable = true }
+    opts.indent = { enable = true }
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, {
+        -- latex
         "bibtex",
         "latex",
+        -- documents
+        "markdown",
+        "json",
         -- vim
         "vim",
         "vimdoc",
+        -- web
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
         -- languages
         "lua",
-        "html",
-        "css"
+        "go",
+        "python",
+        "java",
+        "c",
+        "cpp",
+        "rust"
       })
     end
     if type(opts.highlight.disable) == "table" then
