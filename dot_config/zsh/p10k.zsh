a617c2fe2f795ab30e381d34b0324252e4c05026
Daylam Tayari <git@tayari.gg>
2025-10-14 21:01:13 -0500

    Removed a random s in p10k config

--- dot_config/zsh/p10k.zsh
+++ dot_config/zsh/p10k.zsh
@@ -7,7 +7,7 @@
 
 () {
   emulate -L zsh -o extended_glob
-s
+
   # Unset all configuration options. This allows you to apply configuration changes without
   # restarting zsh. Edit ~/.p10k.zsh and type `source ~/.p10k.zsh`.
   unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
