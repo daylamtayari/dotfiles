2663f8ff9372f517c15b4a0dd7f2689ea16d03b9
Daylam Tayari <git@tayari.gg>
2025-10-18 16:50:48 -0500

    Reconfigured nvim dashboard

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -8,11 +8,74 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
-		dashboard = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "p",
+						desc = "Projects",
+						action = ":lua Snacks.dashboard.pick('projects')",
+					},
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{
+						icon = " ",
+						key = "u",
+						desc = "Update Plugins",
+						action = ":Lazy update",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				header = [[
+                                                               .____   __ _
+  __o__   _______ _ _  _                                     /     /
+  \    ~\                                                  /      /
+    \     '\                                         ..../      .'
+     . ' ' . ~\                                      ' /       /
+    .  _    .  ~ \  .+~\~ ~ ' ' " " ' ' ~ - - - - - -''_      /
+   .  <#  .  - - -/' . ' \  __                          '~ - \
+    .. -           ~-.._ / |__|  ( )  ( )  ( )  0  o    _ _    ~ .
+  .-'                                               .- ~    '-.    -.
+ <                      . ~ ' ' .             . - ~             ~ -.__~_. _ _
+   ~- .               .          . . . . ,- ~
+         ' ~ - - - - =.   <#>    .         \.._
+                     .     ~      ____ _ .. ..  .- .
+                      .         '        ~ -.        ~ -.
+                        ' . . '               ~ - .       ~-.
+                                                    ~ - .      ~ .
+                                                           ~ -...0..~. ____]],
+			},
+			sections = {
+				{ section = "header", hl = "SnacksDashboardNormal" },
+				{ section = "keys", gap = 1, padding = 1 },
+			},
+		},
 		explorer = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
-		picker = { enabled = true, help = false },
+		picker = { enabled = true, help = false, matcher = { frecency = true } },
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
